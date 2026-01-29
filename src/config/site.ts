68a3dcfdd67321b7d7dd082f4a46594350b44fe6
Daud Nugraha <daudelangnugraha@gmail.com>
2026-01-29 16:28:14 +0800

    firm changes

--- src/config/site.ts
+++ src/config/site.ts
@@ -19,7 +19,8 @@ export const siteConfig = {
     prefix: "Technician | Programmer | Former Chief Technology Officer",
     name: "Daud Elang Nugraha",
     intro: "I am a tech specialist with a passion for development and practical solutions. Welcome to my portfolio!",
-    avatar: "/image/cleanavatar.png",
+    avatar: "/image/cleanavatar3.png",
+    avataralt: "/image/cleanavatar.png",
     buttons: [
       { text: "My Profile", url: "https://www.linkedin.com/in/geeztjeez/" },
       { text: "Contact Me", url: "/contact" },
